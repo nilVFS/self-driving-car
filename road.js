b0dc2156b42fc1d3606003b2845cd64c4a1abc1e
Владислав Низьев <vladislavnizev@MacBook-Air-Vladislav.local>
2022-08-05 12:41:49 +0300

    add road borders and road moving effect

--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road {
-    constructor(x, width, laneCount = 4 ) {
+    constructor(x, width, laneCount = 3) {
         this.x = x;
         this.width = width;
         this.laneCount = laneCount;
@@ -9,37 +9,49 @@ class Road {
 
         const infinity = 99999999; // Встроенный метод приводит к багам
         this.top = -infinity;
-        this.bottom = infinity; 
+        this.bottom = infinity;
+
+        const topLeft = {x: this.left, y: this.top};
+        const topRight = {x: this.right, y: this.top};
+        const bottomLeft = {x: this.left, y: this.bottom};
+        const bottomRight = {x: this.right, y: this.bottom};
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ]
     }
 
     getLaneCenter(laneIndex) {
         const laneWidth = this.width / this.laneCount;
-        return this.left + laneWidth / 2 + laneIndex * laneWidth;
+        return this.left + laneWidth / 2 + Math.min(laneIndex, this.laneCount - 1) * laneWidth;
     }
 
     draw(ctx) {
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
-        for (let i = 0; i <= this.laneCount; i++) {
+        for (let i = 1; i <= this.laneCount - 1; i++) {
             const x = lerp(
                 this.left,
                 this.right,
                 i / this.laneCount
             );
 
-            // Добавляет пробелы на линии
-            if (i > 0 && i < this.laneCount) {
-                ctx.setLineDash([20, 20]); // добавляет пробелы по 20 пикселей каждые 20 пикселей
-            } else {
-                ctx.setLineDash([]) // сплошные боковые линии
-            }
+            ctx.setLineDash([20, 20]); // добавляет пробелы по 20 пикселей каждые 20 пикселей
 
             ctx.beginPath();
             ctx.moveTo(x, this.top);
             ctx.lineTo(x, this.bottom);
             ctx.stroke();
         }
+
+        ctx.setLineDash([]);
+        this.borders.forEach(border => {
+            ctx.beginPath();
+            ctx.moveTo(border[0].x, border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke()
+        })
 
     }
 }
