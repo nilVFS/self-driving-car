cdba58edacd9f4f4fa343a494b7e329bc4e7830f
Владислав Низьев <vladislavnizev@MacBook-Air-Vladislav.local>
2022-08-05 11:42:30 +0300

    add moving mechanics

--- car.js
+++ car.js
@@ -8,11 +8,16 @@ class Car {
         this.speed = 0;
         this.acceleration = 0.2;
         this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
 
         this.controls = new Controls();
     }
 
     update() {
+
+        //Движение вперед
+
         if (this.controls.forward) {
             this.speed += this.acceleration; 
         }
@@ -21,17 +26,57 @@ class Car {
             this.speed -= this.acceleration;
         }
 
-        this.y -= this.speed;
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2; 
+        }
+
+        if(this.speed > 0) {
+            this.speed -= this.friction;
+        }
+
+        if (this.speed < 0) {
+            this.speed += this.friction;
+        }
+
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+
+        // Движение по сторонам
+
+        if (this.speed !== 0) { // предотвращение поворота на месте без движения вперед/назад
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.left) {
+                this.angle += 0.03 * flip; 
+            }
+    
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip; 
+            }
+        }
+
+
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
     }
 
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            - this.width / 2,
+            - this.height / 2,
             this.width,
             this.height
         )
         ctx.fill();
+
+        ctx.restore();
     }
 }
